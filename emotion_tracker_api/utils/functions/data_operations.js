563eb6ca963ab8d606fae75dd36ab9ce502cf4bc
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2024-03-11 13:43:24 +0000

    test product

--- emotion_tracker_api/utils/functions/data_operations.js
+++ emotion_tracker_api/utils/functions/data_operations.js
@@ -1,4 +1,4 @@
-const { body, check, validationResult } = require('express-validator');
+const { body, check} = require('express-validator');
 //https://github.com/validatorjs/validator.js?tab=readme-ov-file#sanitizers
 //https://github.com/express-validator/express-validator/issues/1137
 //https://express-validator.github.io/docs/6.6.0/sanitization/
