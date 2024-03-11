eebdec9974d59ffa9c5b2987df823bcd8b71053f
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2024-03-11 14:28:00 +0000

    remove unused folders

--- emotion_tracker_app/controller/views_controller.js
+++
@@ -1,4 +0,0 @@
-// put logic that prepares data specifically for the view here
-// like formatting or deciding which data goes into which part of view
-// this controller should only be responsible for delivering views to 
-// the client and not handling API logic
